f77c53c873049f6dc69afd4ebc7b2b87094494c5
Anton Stambler <anstambler@gmail.com>
2025-11-18 20:55:51 +0200

    useEffect

--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -1,9 +1,33 @@
-import {openingCrawl} from "../utils/constants.js";
+import {base_url, openingCrawl} from "../utils/constants.js";
+import {useEffect, useState} from "react";
 
 const OpeningCrawl = () => {
-    return (
-        <p className="far-galaxy">{openingCrawl}</p>
-    );
+
+    const [openingCrawl, setOpeningCrawl] = useState();
+
+    useEffect(() => { // srabotaet posle togo kak komponent budet smontirovan
+        const episode = Math.floor(Math.random() * 6) + 1
+        fetch(`${base_url}/v1/films/${episode}`)
+            .then(res => res.json())
+            .then(data => setOpeningCrawl(data.opening_crawl))
+            .catch(() => setOpeningCrawl('Error loading opening crawl'));
+        // return () => console.log('Component OpeningCrawl unmounted'); // 4to delaem pri razmontirovanii komponenta
+    }, []) // deps: [] - massiv zavisimosti, esli on pustoy to useEffect srabativaet odin raz posle montirovaniya komponentov
+
+    if (openingCrawl) {
+        return (
+            <p className="far-galaxy">{openingCrawl}</p>
+        );
+    } else {
+        return (
+            <p className="far-galaxy">
+                <span className={'spinner-border sm spinner-border'}></span>
+                <span className={'spinner-grow spinner-grow-sm'}>Loading</span>
+            </p>
+        )
+    }
+
+
 };
 
 export default OpeningCrawl;
